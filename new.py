3f3ddc5bcf83ae0c072eea37ad0f251d412d0ad4
iamkbhavya <119159358+iamkbhavya@users.noreply.github.com>
2023-04-23 00:10:49 +0530

    Update new.py

--- new.py
+++ new.py
@@ -11,7 +11,7 @@ SLV = st.slider("Silver price",0.1,5.8)
 EUR_USD = st.slider("Euro to USD conversion",0.1,5.8)
 
 def predict():
-    float_features = [float(x) for x in [SPX, USO, SLV, EUR/USD]]
+    float_features = [float(x) for x in [SPX, USO, SLV, EUR_USD]]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
     label = prediction[0]
