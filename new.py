689a03b69acb6e8eaae0d60a5d28dd38db6d9535
iamkbhavya <119159358+iamkbhavya@users.noreply.github.com>
2023-04-23 01:07:59 +0530

    Update new.py

--- new.py
+++ new.py
@@ -6,10 +6,10 @@ import pickle
 #load model
 model = pickle.load(open('model1.pkl', 'rb'))
 st.title('What is the Gold price?')
-SPX = st.slider("Stocks price",0.1,5.8)
-USO = st.slider("Oil price",0.1,5.8)
-SLV = st.slider("Silver price",0.1,5.8)
-EUR_USD = st.slider("Euro to USD conversion",0.1,5.8)
+SPX = st.slider("Stocks price",676.530029,2872.870117)
+USO = st.slider("Oil price",7.960000,117.480003)
+SLV = st.slider("Silver price",8.850000,47.259998)
+EUR_USD = st.slider("Euro to USD conversion",1.039047,1.598798)
 
 def predict():
     float_features = [float(x) for x in [SPX, USO, SLV, EUR_USD]]
